b21420306f5cb7795cc29351907e9c4aede37279
Dorian Péron <peron@adacore.com>
2024-08-02 12:19:59 +0200

    customization: Add nvim tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,34 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'kyazdani42/nvim-tree.lua',
+    event = 'VimEnter',
+    config = function()
+      require('nvim-tree').setup()
+      local api = require 'nvim-tree.api'
+      local toggle_cmd = function()
+        api.tree.toggle {
+          find_file = false,
+          focus = true,
+          path = {},
+          update_root = false,
+        }
+      end
+
+      vim.keymap.set('n', '<leader>n', toggle_cmd, { desc = 'SAUCISSE' })
+      require('which-key').setup()
+      require('which-key').add {
+        { '<leader>n', desc = 'Toggle [N]vimTree', mode = { 'n' } },
+      }
+    end,
+    cmd = {
+      'NvimTreeClipboard',
+      'NvimTreeFindFile',
+      'NvimTreeOpen',
+      'NvimTreeRefresh',
+      'NvimTreeToggle',
+    },
+  },
+}
